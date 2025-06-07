fb13d37595a080899445f8aa599d4bf46a8c3515
j-simpson01 <prosier.peseta_05@icloud.com>
2025-06-07 23:22:04 +0100

    Adding get articles list to tecdoc file.

--- data/auto_parts/tecdoc.py
+++ data/auto_parts/tecdoc.py
@@ -16,6 +16,9 @@ from dotenv import load_dotenv
 # 4) Get Category v3
 # GET /category/category-products-groups-variant-3/{vehicleId}/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}/type-id/{typeId}
 
+# 5) Get Articles List
+# GET /articles/list/vehicle-id/{vehicleId}/product-group-id/{productGroupId}/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}/type-id/{typeId}
+
 
 # Example TOYOTA RAV 4 V
 langId = "4" # English (GB)
@@ -24,8 +27,10 @@ modelSeriesId = "39268" # RAV 4 V (_A5_, _H5_)
 typeId = "1" # Automobile
 countryFilterId = "91" # Great Britain
 vehicleId = "140099" # 2.5 Hybrid AWD (AXAH54)
+productGroupId = "100030"
 
-url = f"https://tecdoc-catalog.p.rapidapi.com/category/category-products-groups-variant-3/{vehicleId}/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}/type-id/{typeId}"
+
+url = f"https://tecdoc-catalog.p.rapidapi.com/articles/list/vehicle-id/{vehicleId}/product-group-id/{productGroupId}/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}/type-id/{typeId}"
 
 load_dotenv()
 headers = {
@@ -33,8 +38,60 @@ headers = {
 	"x-rapidapi-host": "tecdoc-catalog.p.rapidapi.com"
 }
 
-
-
 response = requests.get(url, headers=headers)
 
 print(response.json())
+
+
+# '100006': {
+# 	'text': 'Braking System',
+# 	'children': {
+# 		'100027': {
+# 			'text': 'Brake Caliper',
+# 			'children': {
+# 				'100807': {
+# 					'text': 'Brake Caliper Mounting',
+# 					'children': []
+# 				},
+# 				'100806': {
+# 					'text': 'Brake Caliper Parts',
+# 					'children': []
+# 				}
+# 			}
+# 		},
+# 		'102208': {
+# 			'text': 'Brake Fluid',
+# 			'children': []
+# 		},
+# 		'100035': {
+# 			'text': 'Brake Hoses',
+# 			'children': []
+# 		},
+# 		'100626': {
+# 			'text': 'Disc Brake',
+# 			'children': {
+# 				'100630': {
+# 					'text': 'Accessories/Parts',
+# 					'children': []
+# 				},
+# 				'100032': {
+# 					'text': 'Brake Disc',
+# 					'children': []
+# 				},
+# 				'100030': {
+# 					'text': 'Brake Pad',
+# 					'children': []
+# 				}
+# 			}
+# 		},
+# 		'102224': {
+# 			'text': 'High Performance Brakes',
+# 			'children': {
+# 				'102225': {
+# 					'text': 'High Performance Brake Kit',
+# 					'children': []
+# 				}
+# 			}
+# 		}
+# 	}
+# },
