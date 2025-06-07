21d634dfb944bd2804bbf943bec58aae0d13abe1
j-simpson01 <prosier.peseta_05@icloud.com>
2025-06-07 21:59:16 +0100

    Updates to the TecDoc API file.

--- data/auto_parts/tecdoc.py
+++ data/auto_parts/tecdoc.py
@@ -2,7 +2,30 @@ import requests
 import os
 from dotenv import load_dotenv
 
-url = "https://tecdoc-catalog.p.rapidapi.com/languages/list"
+#API calls
+
+# Get All Manufacturers by Type ID, Language ID & Country ID
+# GET /manufacturers/list/lang-id/4/country-filter-id/91/type-id/1"
+
+# 2) Get Models List by Manufacturer ID
+# GET /models/list/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}/type-id/{typeId}
+
+# 3) Get All Vehicle Engine Types
+# GET /types/list-vehicles-types/{modelSeriesId}/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}/type-id/{typeId}
+
+# 4) Get Category v3
+# GET /category/category-products-groups-variant-3/{vehicleId}/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}/type-id/{typeId}
+
+
+# Example TOYOTA RAV 4 V
+langId = "4" # English (GB)
+manufacturerId = "111" # TOYOTA
+modelSeriesId = "39268" # RAV 4 V (_A5_, _H5_)
+typeId = "1" # Automobile
+countryFilterId = "91" # Great Britain
+vehicleId = "140099" # 2.5 Hybrid AWD (AXAH54)
+
+url = f"https://tecdoc-catalog.p.rapidapi.com/category/category-products-groups-variant-3/{vehicleId}/manufacturer-id/{manufacturerId}/lang-id/{langId}/country-filter-id/{countryFilterId}/type-id/{typeId}"
 
 load_dotenv()
 headers = {
