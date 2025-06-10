6273ba17d40e368cf7fa554e1f6deb2450126221
j-simpson01 <prosier.peseta_05@icloud.com>
2025-06-10 21:04:38 +0100

    fastAPI updates to main.

---
+++ FastAPI/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import os
+from FastAPI.data.auto_parts.tecdoc import get_articles_list
+from FastAPI.data.auto_parts.transform import transform_data_articles_list
+from FastAPI.data.auto_parts.load import insert_article_data_into_neo4j
+
+app = FastAPI()
+
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
+
+items = []
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item)
+    return items
+
+@app.get("/items", response_model=list[Item])
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+@app.get("/items/{item_id}", response_model=Item)
+def get_items(item_id: int) -> Item:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+
+load_dotenv()
+URI = os.getenv("NEO4J_URI")
+USER = os.getenv("NEO4J_USERNAME")
+PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+def get_driver(uri, user, password):
+    return GraphDatabase.driver(uri, auth=(user, password))
+
+def main():
+    driver = get_driver(URI, USER, PASSWORD)
+
+    # data = fetch_categories_data("140099", "111")
+    # nodes, edges = parse_categories(data)
+    # load_into_neo4j(nodes, edges)
+
+    article_list = get_articles_list("0 986 495 169")
+    transformed_article_list = transform_data_articles_list(article_list)
+    insert_article_data_into_neo4j(driver, transformed_article_list)
+
+
+if __name__ == "__main__":
+    main()
