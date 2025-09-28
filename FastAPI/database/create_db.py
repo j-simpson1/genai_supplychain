bd6dc8310e1e6173f3475120ff9a08213f933e19
j-simpson01 <prosier.peseta_05@icloud.com>
2025-09-28 15:05:54 +0100

    Deleting database files no longer needed.

--- FastAPI/database/create_db.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel
-from FastAPI.database.database import engine
-from FastAPI.database.models import Manufacturers, Models, Vehicle, Parts, ArticleVehicleLink, Articles, Suppliers
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-if __name__ == "__main__":
-    create_db_and_tables()
--- FastAPI/database/database.py
+++
@@ -1,4 +0,0 @@
-from sqlmodel import create_engine
-
-DATABASE_URL = "postgresql://devuser:devpass@localhost:5432/devdb"
-engine = create_engine(DATABASE_URL, echo=True)
